3be953c60b17ca05c708c514c0b23f3be9b7a886
Marcelo Santos <marcelosantos220716@gmail.com>
2024-07-25 12:25:58 -0300

    Format code
    - Replace double quotes with single quotes

--- src/pages/quiz-page/index.tsx
+++ src/pages/quiz-page/index.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import { Check, X } from 'lucide-react';
-import { QuizOption } from './components/quiz-option';
-import { questions } from '../../mocks/questions';
-import { questionThemes } from '../../lib/question-themes';
-import { QuestionTheme } from '../../types/types';
-import { Timer } from './components/timer';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Check, X } from "lucide-react";
+import { QuizOption } from "./components/quiz-option";
+import { questions } from "../../mocks/questions";
+import { questionThemes } from "../../lib/question-themes";
+import { QuestionTheme } from "../../types/types";
+import { Timer } from "./components/timer";
+import { useNavigate } from "react-router-dom";
 
 interface AnswerOption {
   id: number;
@@ -57,14 +57,6 @@ function QuizPage() {
   };
 
   const handleSkipQuestion = () => {
-    // if (
-    //   !skippedQuestions.find((answerIndex) => answerIndex == currentQuestion)
-    // ) {
-    //   console.log(
-    //     `currentQuestion not exists in skipped questions. ${currentQuestion}`,
-    //   );
-    //   setSkippedQuestions([...skippedQuestions, currentQuestion]);
-    // }
     setSkippedQuestions((prev) => [...prev, currentQuestion]);
     handleNextQuestion();
   };
@@ -114,52 +106,55 @@ function QuizPage() {
   };
 
   const handleTimeOver = () => {
-    console.log('timer over');
+    console.log("timer over");
     handleGameFinish();
   };
 
   const handleGameFinish = () => {
-    navigate('/quiz-result', { state: { answers: answers } });
+    navigate("/quiz-result", { state: { answers: answers } });
   };
 
   const currentQuestionTheme = getQuestionTheme(
-    questions[currentQuestion].theme,
+    questions[currentQuestion].theme
   );
 
   return (
-    <div className="w-full flex flex-col items-center justify-start relative">
-      <div className="flex text-white bg-blueGradient w-full h-28 justify-center">
-        <div className="flex w-2/4 h-full items-center justify-between">
-          <div className="flex gap-8">
-            <div className="flex items-center gap-2">
+    <div className='w-full flex flex-col items-center justify-start relative'>
+      <div className='flex text-white bg-blueGradient w-full h-28 justify-center'>
+        <div className='flex w-2/4 h-full items-center justify-between'>
+          <div className='flex gap-8'>
+            <div className='flex items-center gap-2'>
               <Check />
-              <div className="flex items-baseline gap-2">
-                <p className="font-semibold">{correctAnswers}</p>
-                <p className="text-sm">CORRECT</p>
+              <div className='flex items-baseline gap-2'>
+                <p className='font-semibold'>{correctAnswers}</p>
+                <p className='text-sm'>CORRECT</p>
               </div>
             </div>
-            <div className="flex items-center gap-2">
+            <div className='flex items-center gap-2'>
               <X />
-              <div className="flex items-baseline gap-2">
-                <p className="font-semibold">{wrongAnswers}</p>
-                <p className="text-sm">WRONG</p>
+              <div className='flex items-baseline gap-2'>
+                <p className='font-semibold'>{wrongAnswers}</p>
+                <p className='text-sm'>WRONG</p>
               </div>
             </div>
           </div>
-          <Timer totalDuration={90} handleTimeOver={handleTimeOver} />
+          <Timer
+            totalDuration={90}
+            handleTimeOver={handleTimeOver}
+          />
         </div>
       </div>
-      <div className="bg-white w-2/4 h-max p-8 rounded-sm absolute top-20 shadow-cardShadow">
-        <div className="flex flex-col gap-4 sm:gap-2 lg:gap-6 xl:gap-8 2xl:gap-12">
-          <div className="flex flex-col px-4 gap-2">
-            <div className="flex items-center justify-between">
-              <p className="text-gray-400">{`Question ${currentQuestion + 1} (${questions.length} remaining)`}</p>
-              <div className="flex gap-1">
+      <div className='bg-white w-2/4 h-max p-8 rounded-sm absolute top-20 shadow-cardShadow'>
+        <div className='flex flex-col gap-4 sm:gap-2 lg:gap-6 xl:gap-8 2xl:gap-12'>
+          <div className='flex flex-col px-4 gap-2'>
+            <div className='flex items-center justify-between'>
+              <p className='text-gray-400'>{`Question ${currentQuestion + 1} (${questions.length} remaining)`}</p>
+              <div className='flex gap-1'>
                 {currentQuestionTheme ? (
                   <div
                     className={`${currentQuestionTheme.color} rounded-md px-2`}
                   >
-                    <p className="text-white text-sm font-semibold">
+                    <p className='text-white text-sm font-semibold'>
                       {currentQuestionTheme.name}
                     </p>
                   </div>
@@ -168,12 +163,12 @@ function QuizPage() {
                 )}
               </div>
             </div>
-            <h1 className="text-gray-500 text-2xl leading-7">
+            <h1 className='text-gray-500 text-2xl leading-7'>
               {questions[currentQuestion].question}
             </h1>
           </div>
-          <div className="flex flex-col gap-1">
-            <div className="text-gray-400 px-4">
+          <div className='flex flex-col gap-1'>
+            <div className='text-gray-400 px-4'>
               <p>Answer options</p>
             </div>
             <div>
@@ -190,28 +185,28 @@ function QuizPage() {
                       handleAnswerSelect={handleAnswerSelect}
                     />
                   );
-                },
+                }
               )}
             </div>
           </div>
-          <div className="flex gap-4 items-end justify-center">
+          <div className='flex gap-4 items-end justify-center'>
             {!isAnswerConfirmed ? (
               <>
                 <button
                   onClick={() => {
                     handleSkipQuestion();
                   }}
-                  className="flex-1 py-2 border-2 rounded-lg border-blue-400 text-blue-400 font-medium"
+                  className='flex-1 py-2 border-2 rounded-lg border-blue-400 text-blue-400 font-medium'
                 >
                   Skip
                 </button>
-                <div className="flex flex-1 flex-col items-center">
+                <div className='flex flex-1 flex-col items-center'>
                   {hasSeenAllQuestions && (
                     <button
                       onClick={() => {
                         handleGameFinish();
                       }}
-                      className="w-full text-blue-400"
+                      className='w-full text-blue-400'
                     >
                       Finish Quiz
                     </button>
@@ -220,7 +215,7 @@ function QuizPage() {
                     onClick={() => {
                       confirmAnswer();
                     }}
-                    className="w-full py-2 border-2 rounded-lg border-blue-400 text-white bg-blue-400 font-medium"
+                    className='w-full py-2 border-2 rounded-lg border-blue-400 text-white bg-blue-400 font-medium'
                   >
                     Send Answer
                   </button>
@@ -231,7 +226,7 @@ function QuizPage() {
                 onClick={() => {
                   handleNextQuestion();
                 }}
-                className="flex-1 py-2 border-2 rounded-lg border-blue-400 text-white bg-blue-400 font-medium"
+                className='flex-1 py-2 border-2 rounded-lg border-blue-400 text-white bg-blue-400 font-medium'
               >
                 Next Question
               </button>
--- src/pages/result-page/index.tsx
+++ src/pages/result-page/index.tsx
@@ -10,28 +10,26 @@ const correctAnswerBackground =
 const wrongAnswerBackground =
   'flex w-8 h-8 items-center justify-center text-white bg-redGradient rounded-full';
 
-// const answers = [true,false,true,true,true,true,true,false,true,true,true,true,true,true,true,true,true,true,false,true]
-
 function ResultPage() {
   const state = useLocation().state;
   const answers: boolean[] = state.answers;
 
   return (
-    <div className="w-full flex flex-col items-center justify-start relative">
-      <div className="flex text-white bg-blueGradient w-full h-28 justify-center">
-        <div className="flex w-2/4 h-full items-center justify-between">
+    <div className='w-full flex flex-col items-center justify-start relative'>
+      <div className='flex text-white bg-blueGradient w-full h-28 justify-center'>
+        <div className='flex w-2/4 h-full items-center justify-between'>
           <p>You finished</p>
           <p>00:00</p>
         </div>
       </div>
-      <div className="bg-white w-2/4 h-max p-8 rounded-sm absolute top-20 shadow-cardShadow">
-        <div className="flex flex-col items-center gap-6 lg:gap-6 xl:gap-8 2xl:gap-12">
-          <div className="flex flex-col gap-2 items-center px-4">
-            <h1 className="text-gray-500 text-2xl leading-7">Results</h1>
-            <p className="text-gray-400">Check your results bellow</p>
+      <div className='bg-white w-2/4 h-max p-8 rounded-sm absolute top-20 shadow-cardShadow'>
+        <div className='flex flex-col items-center gap-6 lg:gap-6 xl:gap-8 2xl:gap-12'>
+          <div className='flex flex-col gap-2 items-center px-4'>
+            <h1 className='text-gray-500 text-2xl leading-7'>Results</h1>
+            <p className='text-gray-400'>Check your results bellow</p>
           </div>
 
-          <div className="w-1/2 flex items-start justify-center gap-2 flex-wrap">
+          <div className='w-1/2 flex items-start justify-center gap-2 flex-wrap'>
             {answers.map((answer, index) => {
               return (
                 <button
@@ -46,12 +44,12 @@ function ResultPage() {
             })}
           </div>
 
-          <div className="w-full flex gap-4 items-center justify-between">
+          <div className='w-full flex gap-4 items-center justify-between'>
             <button
               onClick={() => {
                 handleReview();
               }}
-              className="flex-1 py-2 border-2 rounded-lg border-blue-400 text-blue-400 font-medium"
+              className='flex-1 py-2 border-2 rounded-lg border-blue-400 text-blue-400 font-medium'
             >
               Review
             </button>
@@ -59,7 +57,7 @@ function ResultPage() {
               onClick={() => {
                 handleFinish();
               }}
-              className="flex-1 py-2 border-2 rounded-lg border-blue-400 text-white bg-blue-400 font-medium"
+              className='flex-1 py-2 border-2 rounded-lg border-blue-400 text-white bg-blue-400 font-medium'
             >
               Finish
             </button>
